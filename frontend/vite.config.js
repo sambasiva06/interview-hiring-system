95dad97e553a7884fd292b971a3d9a8074c2ddd8
sambasiva06 <psambasivarao03@gmail.com>
2026-05-17 17:33:48 +0530

    chore: setup github pages deployment for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/interview-hiring-system/',
   plugins: [react()],
   server: {
     proxy: {
